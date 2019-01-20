f010c8a7c201378fd4b55fe746a301685f06c312
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2019-01-20 23:10:07 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.listen(process.env.PORT, () =>
+  console.log(`Open localhost:${process.env.PORT}`)
+)
